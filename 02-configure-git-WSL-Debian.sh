d9c5f252877b334444e24c9ccd7336a2485f8a50
phil-gg <45383744+phil-gg@users.noreply.github.com>
2025-12-23 14:29:31 +1000

    Update 02-configure-git-WSL-Debian.sh

--- 02-configure-git-WSL-Debian.sh
+++ 02-configure-git-WSL-Debian.sh
@@ -27,6 +27,27 @@ greenbold=$(printf '\033[92;1m')
 cyanbold=$(printf '\033[96;1m')
 bluebold=$(printf '\033[94;1m')
 
+# Network test
+
+echo -e "\n${bluebold}Testing network connectivity${normal}"
+echo -e "$ wget -q --spider https://raw.githubusercontent.com\
+/${github_username}\
+/${github_project}\
+/${github_branch}\
+/${filename}"
+
+if ! wget -q --spider https://raw.githubusercontent.com\
+/${github_username}\
+/${github_project}\
+/${github_branch}\
+/${filename} 2> /dev/null
+then
+echo "${redbold}> Offline${normal}\n"
+exit 101
+else
+echo "${greenbold}> Online${normal}"
+fi
+
 # Ensure 1password-cli can set sensitive variables
 
 echo -e "\n${cyanbold}Checking whether account registered in 1password-cli\
@@ -47,7 +68,7 @@ RUN THIS NEXT:
 
 eval \$(op account add --signin)
 "
-exit 101
+exit 102
 
 else
 
@@ -67,7 +88,7 @@ eval \$(op signin)
 
 …then re-run this script.
 "
-exit 102
+exit 103
 else
 echo -e "${greenbold}> Logged into 1password-cli${normal}"
 fi
@@ -83,44 +104,23 @@ sgsub5ksyk2khnrzflt4pyziru/\
 z7x5d5r2hdnytyuulwq57lsixa/\
 ucz4cqku2w7ctc3yseteme2boe")
 
-echo -e "\n> GH_TOKEN=$(echo \
-${GH_TOKEN} | head -c8)…\
-$(echo ${GH_TOKEN} | tail -c5)"
+echo -e "> GH_TOKEN=$(echo \
+"${GH_TOKEN}" | head -c8)…\
+$(echo "${GH_TOKEN}" | tail -c5)"
 
 GH_EMAIL=$(op read "op://\
 sgsub5ksyk2khnrzflt4pyziru/\
 z7x5d5r2hdnytyuulwq57lsixa/\
 4w3e6rvo5yag5muvhg3tm5oofi")
 
-echo -e "\n> GH_EMAIL=$(echo ${GH_EMAIL})"
+echo -e "> GH_EMAIL=${GH_EMAIL}"
 
 GH_USERN=$(op read "op://\
 sgsub5ksyk2khnrzflt4pyziru/\
 z7x5d5r2hdnytyuulwq57lsixa/\
 username")
 
-echo -e "\n> GH_USERN=$(echo ${GH_USERN})"
-
-# Network test
-
-echo -e "\n${bluebold}Testing network connectivity${normal}"
-echo -e "$ wget -q --spider https://raw.githubusercontent.com\
-/${github_username}\
-/${github_project}\
-/${github_branch}\
-/${filename}"
-
-if ! wget -q --spider https://raw.githubusercontent.com\
-/${github_username}\
-/${github_project}\
-/${github_branch}\
-/${filename} 2> /dev/null
-then
-echo "${redbold}> Offline${normal}"
-exit 103
-else
-echo "${greenbold}> Online${normal}"
-fi
+echo -e "> GH_USERN=${GH_USERN}"
 
 # Now running `${filename}`
 
@@ -135,7 +135,7 @@ mkdir -p "${HOME}/git/${github_username}/${github_project}"
 
 echo -e "$ cd ~/git/${github_username}/${github_project}"
 cd "${HOME}/git/${github_username}/${github_project}" 2> /dev/null \
-|| { echo -e "  ${redbold}Failed to change directory, exiting${normal}"\
+|| { echo -e "  ${redbold}Failed to change directory, exiting${normal}\n"\
 ; exit 104; }
 
 # Check for presence of git & gh
@@ -164,33 +164,68 @@ fi
 emailcheck=$(git config --global user.email)
 if [ "${emailcheck}" != "${GH_EMAIL}" ];
 then
-echo -e "\n$ git config --global user.email ${GH_EMAIL}"
-git config --global user.email ${GH_EMAIL}
+echo -e "\n$ git config --global user.email \"${GH_EMAIL}\""
+git config --global user.email "${GH_EMAIL}"
 fi
 
 userncheck=$(git config --global user.name)
 if [ "${userncheck}" != "${GH_USERN}" ];
 then
-echo -e "\n$ git config --global user.name ${GH_USERN}"
-git config --global user.name ${GH_USERN}
+echo -e "\n$ git config --global user.name \"${GH_USERN}\""
+git config --global user.name "${GH_USERN}"
 fi
 
 authcheck=$(git config --list | grep -oF "/usr/bin/gh" | sort -u)
 authvalue="/usr/bin/gh"
 if [ "${authcheck}" != "${authvalue}" ];
 then
-echo -e "\n$ echo \${GH_TOKEN} | gh auth login --with-token --hostname \
+echo -e "\n$ echo \"\${GH_TOKEN}\" | gh auth login --with-token --hostname \
 github.com && gh auth setup-git"
-echo ${GH_TOKEN} | gh auth login --with-token --hostname github.com && \
+echo "${GH_TOKEN}" | gh auth login --with-token --hostname github.com && \
 gh auth setup-git
 fi
 
 echo -e "\n$ git config --list\n"
 git config --list
 
-# Sync project to working directory with git
+# Verify gh auth login
+
+echo -e "\n${cyanbold}Verify gh auth login${normal}"
+echo -e "\n$ gh auth status\n"
+gh auth status
+# shellcheck disable=SC2181
+if [ $? -ne 0 ]; then
+echo -e "\n${redbold}> Failed to authenticate GitHub CLI, exiting${normal}\n"
+exit 105
+fi
+
+# Sync present working directory to project remote with git
 
 echo -e "\n${cyanbold}Sync project with github${normal}"
+
+# Clone if no .git folder
+if [ ! -d ".git" ]; then
+echo -e "> .git not created yet\n"
+
+# final dot prevents a duplicate ${github_project} folder
+# clone replaced remote add, which 
+echo -e "\n$ git clone \
+\"https://github.com\
+/${github_username}\
+/${github_project}.git\" .\n"
+git clone "https://github.com/${github_username}/${github_project}.git" .
+
+# force move HEAD (current position) to latest commit in main
+# likely redundant as clone already put HEAD at end of default branch
+echo -e "\n$ git checkout \"${github_branch}\" -f\n"
+git checkout "${github_branch}" -f
+
+# reset --hard synchronises local files to the remote branch state
+# likely redundant as clone probably already updated files this way
+echo -e "\n$ git reset --hard \"origin/${github_branch}\"\n"
+git reset --hard "origin/${github_branch}"
+
+fi
 
 echo -e "\n$ git fetch --porcelain\n"
 git fetch --porcelain
@@ -204,14 +239,13 @@ git init
 echo -e "\n$ git remote add origin \
 https://github.com\
 /${github_username}\
-/${github_project}.git\
-${normal}\n"
+/${github_project}.git\n"
 git remote add origin "https://github.com\
 /${github_username}\
 /${github_project}.git"
 
-echo -e "$ git fetch\n"
-git fetch
+echo -e "\n$ git fetch --porcelain\n"
+git fetch --porcelain
 
 echo -e "\n$ git checkout main -f\n"
 git checkout main -f
@@ -222,12 +256,12 @@ git branch --set-upstream-to "origin/${github_branch}"
 fi
 
 echo -e "\n$ git status\n"
-status=$(git status)
-echo "${status}"
+STATUS=$(git status --porcelain)
+git status
 
 # git pull if status check says it is needed
 
-pullcheck=$(echo "${status}" | grep -oF "use \"git pull\"")
+pullcheck=$(echo "${STATUS}" | grep -oF "use \"git pull\"")
 pullvalue="use \"git pull\""
 if [ "${pullcheck}" = "${pullvalue}" ];
 then
