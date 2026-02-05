38150bd3d0c0a006b8c2a8d9144bb64fafc2d47c
phil-gg <45383744+phil-gg@users.noreply.github.com>
2026-02-06 08:18:20 +1000

    Update 02-configure-git-WSL-Debian.sh

--- 02-configure-git-WSL-Debian.sh
+++ 02-configure-git-WSL-Debian.sh
@@ -27,6 +27,10 @@ greenbold=$(printf '\033[92;1m')
 cyanbold=$(printf '\033[96;1m')
 bluebold=$(printf '\033[94;1m')
 
+# Now running `${filename}`
+
+echo -e "\n${bluebold}Now running ‘${filename}’${normal}"
+
 # Network test
 
 echo -e "\n${bluebold}Testing network connectivity${normal}"
@@ -120,10 +124,6 @@ username")
 
 echo -e "> GH_USERN=${GH_USERN}"
 
-# Now running `${filename}`
-
-echo -e "\n${bluebold}Now running ‘${filename}’${normal}"
-
 # Make folder(s) if they don't exist
 
 if [ ! -d "${HOME}/git/${github_username}/${github_project}/" ]; then
@@ -199,6 +199,14 @@ fi
 echo -e "\n$ git config --list\n"
 git config --list
 
+# Log this latest `Config` operation and display runtime
+
+echo -e "\n${bluebold}${filename} run at${normal}"
+echo -e "> ${runtime}"
+mkdir -p "${HOME}/git/${github_username}/${github_project}"
+echo -e "FILE: ${filename} | EXEC-TIME: ${runtime}" \
+>> "${HOME}/git/${github_username}/${github_project}/config-runs.log"
+
 # Verify gh auth login
 
 echo -e "\n${cyanbold}Verify gh auth login${normal}"
@@ -208,130 +216,10 @@ gh auth status
 if [ $? -ne 0 ]; then
 echo -e "\n${redbold}> Failed to authenticate GitHub CLI, exiting${normal}\n"
 exit 105
-fi
-
-# Connect with GitHub and check status
-
-echo -e "\n${cyanbold}Connect with GitHub and check status${normal}"
-
-# Clone if no .git folder
-if [ ! -d ".git" ]; then
-echo -e "> .git not created yet"
-
-# Clone only works with empty directory: can't have e.g. config-runs.log here
-find "${HOME}/git/${github_username}/${github_project}" -mindepth 1 -delete
-
-# final dot prevents a duplicate ${github_project} folder
-# clone replaced remote add
-echo -e "\n$ git clone \
-\"https://github.com\
-/${github_username}\
-/${github_project}.git\" .\n"
-git clone "https://github.com/${github_username}/${github_project}.git" .
-
-# force move HEAD (current position) to latest commit in main
-# likely redundant as clone already put HEAD at end of default branch
-echo -e "\n$ git checkout \"${github_branch}\" -f\n"
-git checkout "${github_branch}" -f
-
-# reset --hard synchronises local files to the remote branch state
-# likely redundant as clone probably already updated files this way
-echo -e "\n$ git reset --hard \"origin/${github_branch}\"\n"
-git reset --hard "origin/${github_branch}"
-
-fi
-
-# First connect and get remote updates (if .git existed)
-echo -e "\n$ git fetch -v\n"
-git fetch -v
-# shellcheck disable=SC2181
-if [ $? -ne 0 ]; then
-echo -e "\n${redbold}> Failed to fetch project from GitHub, exiting${normal}\n"
-exit 106
-fi
-
-# Show git status, and put porcelain status into a variable
-echo -e "\n$ git status\n"
-STATUS=$(git status -b --porcelain)
-git status
-
-# Log this latest `Config` operation and display runtime
-
-echo -e "\n${bluebold}${filename} run at${normal}"
-echo -e "> ${runtime}"
-mkdir -p "${HOME}/git/${github_username}/${github_project}"
-echo -e "FILE: ${filename} | EXEC-TIME: ${runtime}" \
->> "${HOME}/git/${github_username}/${github_project}/config-runs.log"
-
-# If git is up-to-date, end the script here (no sync actions needed)
-FILES_DIFF=$(git status --porcelain)
-# shellcheck disable=SC1083
-COMMIT_DIFF=$(git rev-list HEAD..@{u} --count)
-if [[ -z "${FILES_DIFF}" ]] && [[ "${COMMIT_DIFF}" -eq 0 ]]; then
-echo -e "${greenbold}> Everything is up to date, exiting${normal}\n"
-exit 0
-fi
-
-# Sync present working directory to project remote with git
-
-echo -e "\n${cyanbold}Sync project with github${normal}"
-
-# Add wholly new (untracked) files
-# The porcelain check '??' identifies untracked files
-if [[ "${STATUS}" == *"??"* ]]; then
-echo -e "\n> New untracked files detected"
-echo -e "\n$ git add .\n"
-git add .
-# Refresh STATUS so the commit message reflects the added state
-STATUS=$(git status -b --porcelain)
-fi
-
-# Commit modifications if the repository is dirty
-if ! git diff --quiet || ! git diff --cached --quiet; then
-
-echo -e "\n> Modified, uncommitted files detected"
-
-# Create commit message
-FILE_COUNT=$(echo "$STATUS" | wc -l)
-if [ "${FILE_COUNT}" -gt 3 ]; then
-COMMIT_MESSAGE="Update ${FILE_COUNT} files"
 else
-COMMIT_MESSAGE="$(echo "${STATUS}" | tr '\n' '' | xargs)"
+echo -e "\n${greenbold}> git & gh configured with GitHub credentials${normal}\n
+Run ‘03-sync-git-WSL-Debian.sh’ next\n"
 fi
-
-echo -e "\n> COMMIT_MESSAGE=\"${COMMIT_MESSAGE}\""
-
-echo -e "\n$ git commit -a -m \"\${COMMIT_MESSAGE}\"\n"
-git commit -a -m "${COMMIT_MESSAGE}"
-
-# Refresh STATUS so the commit message reflects the committed state
-STATUS=$(git status -b --porcelain)
-
-fi
-
-# Pull where necessary
-# Rebase puts any local changes on top of the remote changes (where possible)
-if [[ "${STATUS}" == *"behind"* ]]; then
-echo -e "\n> Pull changes from remote"
-echo -e "\n$ git pull --rebase\n"
-git pull --rebase
-# Refresh STATUS so the commit message reflects the pulled state
-STATUS=$(git status -b --porcelain)
-fi
-
-# Push where necessary
-if [[ "${STATUS}" == *"ahead"* ]]; then
-echo -e "\n> Push local changes to remote"
-echo -e "\n$ git push\n"
-git push
-fi
-
-# If you have made it here, and not exited above, you need status one last time
-echo -e "\n$ git status\n"
-git status
-echo -e "\n${greenbold}> Sync complete${normal}\n"
-
-
 
 ################################################################################
 #
