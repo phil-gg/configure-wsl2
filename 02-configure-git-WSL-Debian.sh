295ebd6ebcf5af1dcdabbd43246e3e2a234f153d
phil-gg <45383744+phil-gg@users.noreply.github.com>
2026-02-06 14:16:24 +1000

    Update 02-configure-git-WSL-Debian.sh

--- 02-configure-git-WSL-Debian.sh
+++ 02-configure-git-WSL-Debian.sh
@@ -122,14 +122,14 @@ echo -e "> GH_USERN=${GH_USERN}"
 
 # Make folder(s) if they don't exist
 
-if [ ! -d "${HOME}/git/${github_username}/${github_project}/" ]; then
-echo -e "$ mkdir -p ~/git/${github_username}/${github_project}"
-mkdir -p "${HOME}/git/${github_username}/${github_project}/"
+if [ ! -d "${HOME}/git/${github_username}/${github_project}" ]; then
+echo -e "\n$ mkdir -p ~/git/${github_username}/${github_project}"
+mkdir -p "${HOME}/git/${github_username}/${github_project}"
 fi
 
 # Navigate to working directory
 
-echo -e "$ cd ~/git/${github_username}/${github_project}"
+echo -e "\n$ cd ~/git/${github_username}/${github_project}"
 cd "${HOME}/git/${github_username}/${github_project}" 2> /dev/null \
 || { echo -e "${redbold}> Failed to change directory, exiting${normal}\n"\
 ; exit 104; }
